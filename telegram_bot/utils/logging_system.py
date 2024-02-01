14b64ebdb566e175616944cd440bdc3a48a5864c
MRX580 <andrejchenko0511@mail.ru>
2024-02-01 22:19:56 +0300

    fix logging system again)))))))))))))))))))

--- telegram_bot/utils/logging_system.py
+++ telegram_bot/utils/logging_system.py
@@ -15,14 +15,16 @@ class TelegramLogsHandler(logging.Handler):
         asyncio.create_task(self.bot.send_message(chat_id=self.chat_id, text=log_entry))
 
 
-def logging_to_file_telegram(log_type, *args):
+def logging_to_file(log_type, *args):
     log_file = 'bot.log'
     logger = logging.getLogger(__name__)
 
+    # Словарь уровней логирования
+    log_levels = {'warn': logging.WARNING, 'info': logging.INFO, 'error': logging.ERROR}
+
+    # Добавляем обработчики, если их еще нет
     if not logger.handlers:
         logger.setLevel(logging.INFO)
-
-        log_levels = {'warn': logging.WARNING, 'info': logging.INFO, 'error': logging.ERROR}
 
         file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, encoding='utf-8')
         file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
@@ -31,10 +33,13 @@ def logging_to_file_telegram(log_type, *args):
         telegram_handler = TelegramLogsHandler(-4180190396)  # Используйте свой chat_id
         logger.addHandler(telegram_handler)
 
+    # Проверка на допустимость типа лога
     if log_type not in log_levels:
         raise ValueError("Недопустимый тип лога: должен быть 'warn', 'info' или 'error'.")
 
+    # Логирование сообщений
     for message in args:
         logger.log(log_levels[log_type], message)
+
 
 
